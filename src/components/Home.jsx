3c335d70ead1c6f14b1832a2dd6499dc17db9746
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-08-29 17:53:00 +0900

    Add meta tags

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import Favicon from 'react-favicon';
+import DocMeta from 'react-doc-meta';
 
+// icons
 import icon from '../images/icon.png';
+import favicon0 from '../images/favicons/picto0.png';
+import favicon1 from '../images/favicons/picto1.png';
+import favicon2 from '../images/favicons/picto2.png';
+import favicon3 from '../images/favicons/picto3.png';
+import favicon4 from '../images/favicons/picto4.png';
+// import favicon0 from '../images/favicons/favicon-0.png';
+// import favicon1 from '../images/favicons/favicon-1.png';
+import ogp from '../images/ogp.png';
+
+const metaTags = [
+    {name: 'og:image', content: ogp}
+];
 
 export default class Home extends React.Component {
     render() {
         return (
             <div id="home">
+                <DocMeta tags={metaTags}/>
+                <Favicon url={[favicon0, favicon1, favicon2, favicon3, favicon4]}/>
+
                 <h1>Welcome to Pictogram Fun !!!</h1>
 
                 <img id="main-icon" src={icon} alt="pictogram.fun main icon"/>
